410f7fc656055b1fed4b67de9e2e75d312ce5c09
Sudhanshu <srsrivastava1926@gmail.com>
2025-08-14 21:29:19 +0530

    Manage Users and Roles

---
+++ src/main/java/com/ecommerce/sb_ecom/model/Address.java
@@ -0,0 +1,60 @@
+package com.ecommerce.sb_ecom.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be at least 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be at least 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters")
+    private String stateName;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be at least 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Pin Code must be at least 2 characters")
+    private String pinCode;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users;
+
+    public Address(String street, String buildingName, String city, String stateName, String country, String pinCode) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.stateName = stateName;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
+}
---
+++ src/main/java/com/ecommerce/sb_ecom/model/User.java
@@ -0,0 +1,73 @@
+package com.ecommerce.sb_ecom.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Fetch;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "password")
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                    joinColumns = @JoinColumn(name = "user_id"),
+                     inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user",
+               cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+               orphanRemoval = true)
+    private Set<Product> products;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    @JoinTable(name = "user_address",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
+
+
+}
